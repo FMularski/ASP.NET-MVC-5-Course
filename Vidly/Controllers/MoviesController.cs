1da2f6b763e82eabe1648eaf4d36d7ece9f52edc
Filip Mularski <filipelblag@gmail.com>
2018-02-16 12:09:02 +0100

    Attribute routing.
    Getting used to attribite routing.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -20,5 +20,26 @@ namespace Vidly.Controllers
 
             return View(movie);
         }
+
+        public ContentResult Edit( int id)
+        {
+            return Content("id = " + id);
+        }
+
+        public ContentResult Params( int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrEmpty(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex = {0}, sortBy = {1}", pageIndex, sortBy));
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ContentResult Released( int year, int month)
+        {
+            return Content(String.Format("year = {0} month = {1}", year, month));
+        }
     }
 }

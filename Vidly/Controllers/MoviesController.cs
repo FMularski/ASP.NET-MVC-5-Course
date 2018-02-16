bc1262f73f8cfda36b44cdbd0984380f2a0fed62
Filip Mularski <filipelblag@gmail.com>
2018-02-16 15:44:20 +0100

    Exercise 2
    + /customers/
    + /customers/details/1
    + /movies/

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -12,7 +12,13 @@ namespace Vidly.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var movies = new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek"},
+                new Movie { Id = 2, Name = "Wall-e"}
+            };
+
+            return View(movies);
         }
 
         public ViewResult Random()

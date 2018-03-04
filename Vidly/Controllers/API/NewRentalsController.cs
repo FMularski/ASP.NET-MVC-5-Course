2f41a68fd38cc11b8f194305ad17f03b6546a537
Filip Mularski <filipelblag@gmail.com>
2018-03-04 18:12:13 +0100

    Added CreateNewRentals action in NewRentalsController API.

--- Vidly/Controllers/API/NewRentalsController.cs
+++ Vidly/Controllers/API/NewRentalsController.cs
@@ -5,15 +5,45 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Vidly.DTOs;
+using Vidly.Models;
 
 namespace Vidly.Controllers.API
 {
     public class NewRentalsController : ApiController
     {
+        private ApplicationDbContext _Context;
+
+        public NewRentalsController()
+        {
+            _Context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _Context.Dispose();
+        }
+
         [HttpPost]
         public IHttpActionResult CreateNewRentals( NewRentalDTO newRental)
         {
-            throw new NotImplementedException();
+            var customer = _Context.Customers.Single(c => c.Id == newRental.CustomerId);
+            var movies = _Context.Movies.Where(m => newRental.MovieIds.Contains(m.Id));
+
+            foreach (var movie in movies)
+            {
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+
+                _Context.Rentals.Add(rental);
+            }
+
+            _Context.SaveChanges();
+
+            return Ok();
         }
     }
 }

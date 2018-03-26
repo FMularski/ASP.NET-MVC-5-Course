6e28d23bdc1b2cde06dc52876498f264e5999f76
Filip Mularski <filipelblag@gmail.com>
2018-03-26 22:55:33 +0200

    Deployment stuff
    Added new publish profile. Used Configuration.AppSettings in Startup.Auth.cs

--- Vidly/App_Start/Startup.Auth.cs
+++ Vidly/App_Start/Startup.Auth.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
@@ -55,8 +56,8 @@ namespace Vidly
             //   consumerSecret: "");
 
             app.UseFacebookAuthentication(
-               appId: "2107132009541890",
-               appSecret: "d4dc7d13e2b30376be6651fd46309234");
+               appId: ConfigurationManager.AppSettings["FacebookAppId"],
+               appSecret: ConfigurationManager.AppSettings["FacebookAppSecret"]);
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
